dc77981c0e01c433294160f1c40959fab86e6442
Dominik Kesim <>
2019-01-17 12:51:04 +0100

    enabled mongodb and provided a docker-compose file as well as setting up a separate container for mongodb, spring container is not booting properly

--- spring-data/src/main/java/com/codingdays/spring/springdata/controllers/exceptionhandlers/CustomersNotFoundException.java
+++ spring-data/src/main/java/com/codingdays/spring/springdata/controllers/exceptionhandlers/CustomersNotFoundException.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * @author dominik on 06.01.19
- * @project spring-data
  */
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "There are no customers registered yet!")
 public class CustomersNotFoundException extends RuntimeException {
--- spring-data/src/test/java/com/codingdays/spring/springdata/CustomerControllerTest.java
+++ spring-data/src/test/java/com/codingdays/spring/springdata/CustomerControllerTest.java
@@ -26,7 +26,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 /**
  * @author dominik on 14.01.19
- * @project spring-data
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(CustomerController.class)
