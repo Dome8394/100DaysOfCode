eebc5be2abc8f5ab3a335ba6ec8f68e7c8a31f46
Dominik Kesim <Dominik.Kesim@gmail.com>
2019-03-12 08:25:16 +0100

    added unit test for post method but still failes

--- spring-data/src/test/java/com/codingdays/spring/springdata/controller/CustomerControllerTest.java
+++ spring-data/src/test/java/com/codingdays/spring/springdata/controller/CustomerControllerTest.java
@@ -3,13 +3,17 @@ package com.codingdays.spring.springdata.controller;
 import com.codingdays.spring.springdata.business.CustomerBusinessService;
 import com.codingdays.spring.springdata.controllers.customers.CustomerController;
 import com.codingdays.spring.springdata.entities.CustomerEntity;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +23,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Arrays;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -106,6 +111,32 @@ public class CustomerControllerTest {
                 .andReturn();
 
         JSONAssert.assertEquals(expectedNull, result.getResponse().getContentAsString(), false);
+    }
+
+    /**
+     * Testing post method
+     * @throws Exception
+     */
+    @Test
+    public void saveCustomerTest_basic() throws Exception {
+
+        CustomerEntity customerEntity = new CustomerEntity("Will", "Smith", 1);
+        JSONArray jsonObject = new JSONArray("[{\"firstName\": Will, \"lastName\": \"Smith\", \"id\": 1}]");
+
+        when(businessService.saveCustomer(Mockito.any(CustomerEntity.class))).thenReturn(
+                customerEntity.toString()
+        );
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/api/customer/new")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .content(jsonObject.toString());
+
+        MvcResult result = mock.perform(request)
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        assertEquals(HttpStatus.CREATED.value(), result.getResponse().getStatus());
     }
 
 
