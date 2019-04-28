369c100a19f5eff336ccc2f79a1c57ea9f9739d9
Dominik Kesim <Dominik.Kesim@gmail.com>
2019-04-28 10:38:31 +0200

    integrated product entity

--- products/src/main/java/springdata/products/controller/ProductController.java
+++
@@ -1,8 +0,0 @@
-package springdata.products.controller;
-
-/**
- * @author dominik on 27.01.19
- * @project products
- */
-public class ProductController {
-}
--- products/src/main/java/springdata/products/repositories/ProductEntityRepository.java
+++
@@ -1,8 +0,0 @@
-package springdata.products.repositories;
-
-/**
- * @author dominik on 27.01.19
- * @project products
- */
-public class ProductEntityRepository {
-}
