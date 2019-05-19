d19c77ad1507dd64d9b97c41e326da4c05d02f2d
Dominik Kesim <Dominik.Kesim@gmail.com>
2019-05-19 20:11:03 +0200

    last fixes for master merge

--- products/src/main/java/springdata/products/controller/ProductController.java
+++ products/src/main/java/springdata/products/controller/ProductController.java
@@ -3,7 +3,6 @@ package springdata.products.controller;
 import javafx.application.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import springdata.products.entities.Product;
 import springdata.products.services.ProductService;
